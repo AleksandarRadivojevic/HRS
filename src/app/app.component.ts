0b9feb0fe31d253bd0127fbdacd53b9fd527df24
Aleksandar Radivojevic <aleksandar.radivojevic@hotmail.com>
2024-04-22 16:27:41 +0200

    Added syncfusion scheduler to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DayService, WeekService, WorkWeekService, MonthService, AgendaService, ScheduleModule } from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ScheduleModule],
+  providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXtccnRcR2ldWUFxV0o=');
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
