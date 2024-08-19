f92006346767025504ce5ed401ea6940e106a0bb
Milan Gajic <milan.gajic@qinshift.com>
2024-08-19 22:37:45 +0200

    Adds ErrorHandlerService and Modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core';
+import { HeaderComponent } from './core/component';
+import { ErrorHandlerService } from './core/services/error-handler.service';
+import { ModalComponent } from './shared/modal/modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'house_reservation_app';
+  errorHandlerService = inject(ErrorHandlerService);
+
+  errorPopupMessage = computed<string>(() => this.errorHandlerService.popupMessage());
+
+  public onCloseDialog(): void {
+    this.errorHandlerService.clearPopupMessage();
+  }
 }
